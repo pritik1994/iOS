2a447c004184d1e2149ccc6d80e0c39fbbe69a4b
priti kanaujiya <priti.kanaujiya@paytm.com>
2019-02-17 12:21:34 +0530

    hh

---
+++ hh/AVBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AVBViewController.swift
+//  hh
+//
+//  Created by Priti Kanaujiya on 17/02/19.
+//  Copyright © 2019 Priti Kanaujiya. All rights reserved.
+//
+
+import UIKit
+
+class AVBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+print("AVBViewController")    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
