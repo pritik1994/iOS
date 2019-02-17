8a3289cbe5f72c60e7724a4cf1d635e6e6ef9c53
priti kanaujiya <priti.kanaujiya@paytm.com>
2019-02-17 12:34:58 +0530

    kkk

--- hh/AViewController.swift
+++ hh/AViewController.swift
@@ -16,9 +16,7 @@ class AViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        labelFirst.text = "The first thing you’ll fix by using a stack view is the spacing between the bottom row of buttons. A stack view can distribute its views along its axis in various ways, and one of the ways is with an equal amount of spacing between each view"
-        labelSecond.text = "f the outline view isn’t already open, go ahead and open it by using the Show Document Outline button at the bottom left of the storyboard canva"
-        labelThird.text = "In case they aren’t all selected, you can also Command-click on each button in the outline view to select them"
+        labelFirst.text = "priti"
     }
     
 
